d3076088282376ea3f437303597e480587d822f4
liweidong <abc2192233@163.com>
2021-08-23 03:47:40 -0700

    First Commit

--- src/prophet/Prophet_Dev.py
+++ src/prophet/Prophet_Dev.py
@@ -5,23 +5,25 @@ import plotly.io as pio
 import redis
 import pickle
 import pandas as pd
+import json
+from prophet.serialize import model_to_json, model_from_json
 
 r = redis.StrictRedis(host='m.cdh', password='123456', port='6379', db=0)
 dat = r.lrange("39.107.26.28", 0, -1)
-
+df = pd.DataFrame(columns=['ds', 'y'])
+i = 0
 for x in dat:
     js = pickle.loads(x)
     timestamp = pd.to_datetime(js['timestamp'], unit='s')
     value = js['value']
-    print(timestamp, value)
+    df.loc[i] = [timestamp, value]
+    i = i + 1
 
+m = Prophet()
+m.fit(df)
+future = m.make_future_dataframe(freq='T', periods=60)
+forecast = m.predict(future)
+print(forecast.tail())
 
-
-# df = pd.read_csv('/root/jupyter/example_wp_peyton_manning.csv')
-#
-# m = Prophet()
-# m.fit(df)
-#
-# future = m.make_future_dataframe(freq='D', periods=24)
-#
-# forecast = m.predict(future)
+with open('/root/PycharmProjects/Prophet_Project/MachineLearningModel/serialized_model.json', 'w') as fout:
+    json.dump(model_to_json(m), fout)  # Save model
